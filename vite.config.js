122de0fdae5414ef09f5fa83bb89a49c110ad407
Сюзяев Артем Александрович <kudirev@mail.ru>
2024-04-18 14:33:08 +0300

    added gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+ publicPath: `/psy-roshka/`,
  plugins: [vue(), svgLoader(), createSvgIconsPlugin({
   iconDirs: [path.resolve(process.cwd(), 'src/app/assets/icons')],
   symbolId: 'icon-[dir]-[name]'
